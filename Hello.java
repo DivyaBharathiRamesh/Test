439f46cdf47d54730321d7d2b2d4676f755633fa
Friend <friend10@gmail.com>
2020-10-09 19:24:56 +0530

    Third Commit

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ class Hello
     public static void main (String[] args)
     {
 		System.out.println("Hello World!!!");
+		System.out.println("Cloud Computing"):
 		System.out.println("Enjoy Learning");
 		System.out.println("Stay Home... Stay Safe");
     }
